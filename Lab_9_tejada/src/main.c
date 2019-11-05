9d90f311bfecabdda7c57a73114e76a4f73e86d1
jltejada98 <tejada@purdue.edu>
2019-11-05 17:24:21 -0500

    Lab 09 Completed

--- Lab_9_tejada/src/main.c
+++ Lab_9_tejada/src/main.c
@@ -50,6 +50,37 @@ void I2C1_clearnack(void) {
  */
 void init_I2C1() {
     // Student code goes here
+	RCC->AHBENR |= RCC_AHBENR_GPIOBEN;
+	GPIOB->MODER |= 2<<(2*6) | 2<<(2*7); //ALternate Function Mode
+	GPIOB->AFR[0] |= 1<<(4*6) | 1<<(4*7); //Alternate Function Set
+
+	RCC->APB1ENR |= RCC_APB1ENR_I2C1EN;
+	//CR1 Config from notes:
+	I2C1->CR1 &= ~I2C_CR1_PE;
+	I2C1->CR1 &= ~I2C_CR1_ANFOFF;
+	I2C1->CR1 &= ~I2C_CR1_ERRIE;
+	I2C1->CR1 &= ~I2C_CR1_NOSTRETCH;
+
+	//Continued
+	I2C1->CR2 &= ~I2C_CR2_ADD10;
+	I2C1->CR2 |= I2C_CR2_NACK; //Enable Nack Generation
+
+	//From notes
+	I2C1->TIMINGR =0;
+	I2C1->TIMINGR &= ~I2C_TIMINGR_PRESC;
+	I2C1->TIMINGR |= 4 << 28; //PSC 4
+	I2C1->TIMINGR |= 3 << 20; //SCLDEL is 3
+	I2C1->TIMINGR |= 1 << 16; //SDADEL is 1
+	I2C1->TIMINGR |= 3 << 8; //SCLH is 3
+	I2C1->TIMINGR |= 9 << 0; //SCLL is 9
+
+	//Also from notes
+	I2C1->OAR1 &= ~I2C_OAR1_OA1EN; // Disable own address 1
+	I2C1->OAR1 = I2C_OAR1_OA1EN | 0x2;
+	I2C1->OAR2 &= ~I2C_OAR2_OA2EN; //Disable own address
+
+	//Enable
+	I2C1->CR1 |= I2C_CR1_PE;
 
     //---------End-----------
 }
@@ -61,13 +92,27 @@ void init_I2C1() {
 // See lab document for description
 void I2C1_start(uint8_t addr, uint32_t dir) {
     // Student code goes here
-
+	I2C1->CR2 &= ~(I2C_CR2_SADD);//Clear bits
+	I2C1->CR2 |= (addr << 1) & I2C_CR2_SADD; //Set to address
+	if(dir == RD){
+		I2C1->CR2 |= I2C_CR2_RD_WRN;
+	}
+	else{
+		I2C1->CR2 &= ~I2C_CR2_RD_WRN;
+	}
+	I2C1->CR2 |= I2C_CR2_START;
     //---------End-----------
 }
 
 // See lab document for description
 void I2C1_stop() {
     // Student code goes here
+	if(I2C1->ISR & I2C_ISR_STOPF){
+		return;
+	}
+	I2C1->CR2 |= I2C_CR2_STOP;
+	while(!(I2C1->ISR & I2C_ISR_STOPF));
+	I2C1->ICR |= I2C_ICR_STOPCF;
 
     //---------End-----------
 }
@@ -75,6 +120,28 @@ void I2C1_stop() {
 // See lab document for description
 int I2C1_senddata(uint8_t* data, uint32_t size) {
     // Student code goes here
+	I2C1->CR2 &= ~(I2C_CR2_NBYTES);
+	I2C1->CR2 |= (size << 16) & I2C_CR2_NBYTES; //Something here
+
+	int timeout;
+	for(int i=0; i<size; i++) {
+		timeout = 0;
+		while(!(I2C1->ISR & I2C_ISR_TXIS)){
+			timeout++;
+			if (timeout > 1000000){
+				return FAIL;
+			}
+		}
+		I2C1->TXDR = data[i];
+	}
+	while(!(I2C1->ISR & I2C_ISR_TC) && !(I2C1->ISR & I2C_ISR_NACKF));
+
+	if ((I2C1->ISR & I2C_ISR_NACKF) != 0) {
+		return FAIL;
+	}
+	else{
+		return SUCCESS;
+	}
 
     //---------End-----------
 }
@@ -82,8 +149,31 @@ int I2C1_senddata(uint8_t* data, uint32_t size) {
 // See lab document for description
 int I2C1_readdata(int8_t* data, uint32_t size) {
     // Student code goes here
+	I2C1->CR2 &= ~(I2C_CR2_NBYTES);
+	I2C1->CR2 |= (size << 16) & I2C_CR2_NBYTES;
 
-    //---------End-----------
+	int timeout;
+
+	for(int i=0; i<size; i++) {
+		timeout = 0;
+		while(!(I2C1->ISR & I2C_ISR_RXNE)){
+			timeout++;
+			if (timeout > 1000000){
+				return FAIL;
+			}
+		}
+		data[i] = I2C1->RXDR;
+	}
+
+	while(!(I2C1->ISR & I2C_ISR_TC) && !(I2C1->ISR & I2C_ISR_NACKF));
+
+	if ((I2C1->ISR & I2C_ISR_NACKF) != 0) {
+		return FAIL;
+	}
+	else{
+		return SUCCESS;
+	}
+	//---------End-----------
 }
 
 //===========================================================================
@@ -92,7 +182,15 @@ int I2C1_readdata(int8_t* data, uint32_t size) {
 // See lab document for description
 void write_EEPROM(uint16_t wr_addr, uint8_t data) {
     // Student code goes here
-
+	uint8_t write_buf[3];
+	write_buf[0] = wr_addr >> 8; //Select higher byte
+	write_buf[1] = wr_addr; //select the lower byte
+	write_buf[2] = data;
+	I2C1_waitidle();
+	I2C1_start(0x50,WR); //EEPROM ADDRES: 0x50
+	I2C1_senddata(write_buf, 3);
+	I2C1_stop();
+	micro_wait(5000);
     //---------End-----------
 }
 
@@ -102,13 +200,45 @@ void write_EEPROM(uint16_t wr_addr, uint8_t data) {
 // See lab document for description
 void set_gpio(int pin) {
     // Student code goes here
+	uint8_t write_buf[2];
+	write_buf[0] = 0x0;
+	write_buf[1] = 0 << pin;
+	I2C1_waitidle();
+	I2C1_start(0x20,WR); //MCP23008 ADDRESS: 0x40
+	I2C1_senddata(write_buf,2);
+	I2C1_stop();
+	micro_wait(5000);
 
+	write_buf[0] = 0x9;
+	write_buf[1] = 1 << pin;
+	I2C1_waitidle();
+	I2C1_start(0x20,WR); //MCP23008 ADDRESS: 0x40
+	I2C1_senddata(write_buf,2);
+	I2C1_stop();
+	micro_wait(5000);
     //---------End-----------
 }
 
 // See lab document for description
 void clear_gpio(int pin) {
     // Student code goes here
+	// Student code goes here
+		uint8_t write_buf[2];
+		write_buf[0] = 0x0;
+		write_buf[1] = 0 << pin;
+		I2C1_waitidle();
+		I2C1_start(0x20,WR); //MCP23008 ADDRESS: 0x40
+		I2C1_senddata(write_buf,2);
+		I2C1_stop();
+		micro_wait(5000);
+
+		write_buf[0] = 0x9;
+		write_buf[1] = 0 << pin;
+		I2C1_waitidle();
+		I2C1_start(0x20,WR); //MCP23008 ADDRESS: 0x40
+		I2C1_senddata(write_buf,2);
+		I2C1_stop();
+		micro_wait(5000);
 
     //---------End-----------
 }
@@ -220,8 +350,8 @@ int main(void)
     //test_wiring();
     //prob2();
     //prob3();
-    prob4();
-    //prob5();
+    //prob4();
+    prob5();
 	while(1)
 	    ;
 }
